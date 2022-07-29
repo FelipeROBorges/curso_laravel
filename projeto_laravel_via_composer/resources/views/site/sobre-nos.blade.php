33bdb5c4b423bb9faa045befb80658a05c5c4c9c
felipe.borges <trc.felipe.borges@trc.sebraeba.com.br>
2022-07-29 11:24:30 -0300

    Adicionando agrupamento de rotas e nomenclatura diferente.

--- projeto_laravel_via_composer/resources/views/site/sobre-nos.blade.php
+++ projeto_laravel_via_composer/resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,15 @@
 <h3>Sobre nos (View)</h3>
+
+<!-- Chamando rotas pelo seu apelido -->
+
+<ul>
+    <li>
+       <a href="{{ route('site.index') }}">Pricipal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}">Contato</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}">Sobre Nós</a>
+    </li>
+</ul>
--- projeto_laravel_via_composer/routes/web.php
+++ projeto_laravel_via_composer/routes/web.php
@@ -14,23 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
+// Utilizando o name ao final de uma rota é possivel atribuir um nome a mesma
+// Oque não podifica o caminho mas facilita a chamada e padronização da mesma
+// Evitando assim a dependencia direto entre rotas e nomes de páginas
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
 
+// Agrupamento de rotas atravez do prefixo app e chamada de group
+Route::prefix('/app')->group(function(){ 
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
 
-// Sempre manter aparametros opicionais ao lado direitro
-// Pois se forem definidos a esquerda gera problemas de rota  
-// Ex de erro: {nome?}/{categoria}/{assunto}/{mensagem?}
-
-Route::get('/contato/{nome}/{categoria?}/{assunto?}/{mensagem?}', 
-        function(string $nome,
-                string  $categoria = 'Categoria não informada'
-                        ,string  $mensagem = 'Mensagem não informada',
-                                string  $assunto = 'Assunto não informado') {
-    echo 'Estamos aqui: '.$nome.'<br>';
-    echo 'Essa é sua categoria: '.$categoria.'<br>';
-    echo 'Essa é sua mensagem: '.$mensagem.'<br>';
-    echo 'Essa é sua assunto: '.$assunto.'<br>';
-    
 });
+
+
+/* 
+* Sempre manter aparametros opicionais ao lado direitro
+* Pois se forem definidos a esquerda gera problemas de rota  
+* Ex de erro: {nome?}/{categoria}/{assunto}/{mensagem?}
+*/
+
+// Route::get('/contato/{nome}/{categoria_id}', 
+//         function(
+//             string $nome,
+//             int  $categoria_id = 1
+//         ) {
+//     echo 'Estamos aqui: '.$nome.'<br>';
+//     echo 'Essa é sua categoria: '.$categoria_id;
+
+// })->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+'); // Contrução de espreção regular, que trata erros de tipagem
