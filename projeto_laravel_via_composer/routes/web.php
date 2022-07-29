57926b688941f946f5edb9fb6534cc718fb8b3c4
felipe.borges <trc.felipe.borges@trc.sebraeba.com.br>
2022-07-29 10:45:49 -0300

    Estudo de rotas opicionais e tratativas.

--- projeto_laravel_via_composer/routes/web.php
+++ projeto_laravel_via_composer/routes/web.php
@@ -18,3 +18,19 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
+
+// Sempre manter aparametros opicionais ao lado direitro
+// Pois se forem definidos a esquerda gera problemas de rota  
+// Ex de erro: {nome?}/{categoria}/{assunto}/{mensagem?}
+
+Route::get('/contato/{nome}/{categoria?}/{assunto?}/{mensagem?}', 
+        function(string $nome,
+                string  $categoria = 'Categoria não informada'
+                        ,string  $mensagem = 'Mensagem não informada',
+                                string  $assunto = 'Assunto não informado') {
+    echo 'Estamos aqui: '.$nome.'<br>';
+    echo 'Essa é sua categoria: '.$categoria.'<br>';
+    echo 'Essa é sua mensagem: '.$mensagem.'<br>';
+    echo 'Essa é sua assunto: '.$assunto.'<br>';
+    
+});
